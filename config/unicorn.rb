f4af8c4c5cd10ebf3f58ae05b047dfd21f2ba239
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-03-30 23:46:42 +0800

    change upstream name

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.todo.sock2"
+listen "/tmp/unicorn.todo.sock"
 worker_processes 2
 timeout 30
