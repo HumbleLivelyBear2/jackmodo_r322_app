f488d4aa371f4b343ee5e266395c70288ac0ebd7
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-03-30 15:52:31 +0800

    deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+root = "/home/apps/apps/jackmodo_r322_app/current"
+
+working_directory "/vagrant"
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
