aac98e2f716c95cefc7d954c1ea1abb937b75db1
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-03-31 17:03:44 +0800

    modify config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,51 +1,39 @@
-require "bundler/capistrano"
+require 'bundler/capistrano'
+# require 'hoptoad_notifier/capistrano'
 
-server "106.187.42.93", :web, :app, :db, primary: true
+set :application, "your-project-name"
 
-set :application, "jackmodo"
-set :user, "root"
-set :deploy_to, "/home/deployer2/apps/#{application}"
+set :branch, "master"
+set :repository,  "git@github.com:HumbleLivelyBear2/jackmodo_r322_app.git"
+set :scm, "git"
+set :user, "root" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
+set :port, "22"
+
+set :deploy_to, "/home/rails"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
-set :scm, "git"
-set :repository, "git@github.com:HumbleLivelyBear2/jackmodo_r322_app.git"
-set :branch, "master"
-
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+role :web, "106.187.42.93"
+role :app, "106.187.42.93"
+role :db,  "106.187.42.93", :primary => true
 
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
+  task :copy_config_files, :roles => [:app] do
+    db_config = "#{shared_path}/database.yml"
+    run "cp #{db_config} #{release_path}/config/database.yml"
   end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  
+  task :update_symlink do
+    run "ln -s {shared_path}/public/system {current_path}/public/system"
   end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
+  
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-  before "deploy", "deploy:check_revision"
 end
+
+# after "deploy:update_code", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
+# after "deploy:finalize_update", "deploy:update_symlink" # 如果有實作使用者上傳檔案到public/system，請打開
